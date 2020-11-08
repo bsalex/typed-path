e03349b888d37d797bf123e1d1fa397c6effa1d6
Nick Lvov <nick.lvov.dev@yandex.ru>
2020-11-08 11:16:22 +0700

    additional handlers config

--- index.ts
+++ index.ts
@@ -1,33 +1,3 @@
-export type TypedPathKey = string | symbol | number;
-
-export type TypedPathNode<T> = {
-    $path: string;
-    $raw: TypedPathKey[];
-};
-
-export type TypedPathFunction<T> = (...args: any[]) => T;
-
-export type TypedPathWrapper<T> = (T extends Array<infer Z>
-    ? {
-        [index: number]: TypedPathWrapper<Z>;
-    }
-    : T extends TypedPathFunction<infer RET>
-        ? {
-            (): TypedPathWrapper<RET>;
-        } & {
-            [P in keyof RET]: TypedPathWrapper<RET[P]>;
-        }
-        : {
-            [P in keyof T]: TypedPathWrapper<T[P]>;
-        }
-    ) & TypedPathNode<T>;
-
-const toStringMethods: (string | symbol | number)[] = [
-    'toString',
-    Symbol.toStringTag,
-    'valueOf'
-];
-
 function pathToString(path: string[]): string {
     return path.reduce((current, next) => {
         if (+next === +next) {
@@ -40,22 +10,52 @@ function pathToString(path: string[]): string {
     }, '');
 }
 
-export function typedPath<T>(path: string[] = []): TypedPathWrapper<T> {
-    return <TypedPathWrapper<T>>new Proxy({}, {
+export type TypedPathKey = string | symbol | number;
+
+export type TypedPathFunction<T> = (...args: any[]) => T;
+
+export type TypedPathHandlersConfig = Record<string, <T extends TypedPathHandlersConfig = Record<never, never>>(path: string[], additionalHandlers?: T) => any>;
+
+const defaultHandlersConfig = {
+    $path: (path: string[]) => pathToString(path),
+    $raw: (path: string[]) => path,
+    toString: (path: string[]) => () => pathToString(path),
+    [Symbol.toStringTag]: (path: string[]) => () => pathToString(path),
+    valueOf: (path: string[]) => () => pathToString(path),
+}
+
+export type TypedPathHandlers<T extends TypedPathHandlersConfig> = {
+    [key in keyof T]: ReturnType<T[key]>;
+};
+
+export type TypedPathWrapper<T, TPH extends TypedPathHandlers<Record<never, never>>> = (T extends Array<infer Z>
+    ? {
+        [index: number]: TypedPathWrapper<Z, TPH>;
+    }
+    : T extends TypedPathFunction<infer RET>
+        ? {
+            (): TypedPathWrapper<RET, TPH>;
+        } & {
+            [P in keyof RET]: TypedPathWrapper<RET[P], TPH>;
+        }
+        : {
+            [P in keyof T]: TypedPathWrapper<T[P], TPH>;
+        }
+    ) & TypedPathHandlers<TPH>;
+
+export function typedPath<T, K extends TypedPathHandlersConfig = Record<never, never>>(path: string[] = [], additionalHandlers?: K): TypedPathWrapper<T, K & typeof defaultHandlersConfig> {
+    return <TypedPathWrapper<T, K & typeof defaultHandlersConfig>>new Proxy({}, {
         get(target: T, name: TypedPathKey) {
-            if (name === '$path') {
-                return pathToString(path);
+            const handlersConfig: TypedPathHandlersConfig = { ...(additionalHandlers ?? {}), ...defaultHandlersConfig };
+
+            const keys: (keyof typeof handlersConfig)[] = Object.keys(handlersConfig);
+            for (const key of keys) {
+                if (key === name) {
+                    return handlersConfig[key](path, additionalHandlers);
+                }
             }
 
-            if (name === '$raw') {
-                return path;
-            }
-
-            if (toStringMethods.includes(name)) {
-                return () => pathToString(path);
-            }
-
-            return typedPath([...path, name.toString()]);
+            return typedPath([...path, name.toString()], additionalHandlers);
         }
     });
 }
